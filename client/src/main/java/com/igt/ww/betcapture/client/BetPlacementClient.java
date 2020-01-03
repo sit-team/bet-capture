101289670fcb98eb328a96bb807eb42cbf66bcfe
Cekovic, Mileta <mileta.cekovic@igt.com>
2020-01-03 12:24:04 +0100

    Tweak Client

--- client/src/main/java/com/igt/ww/betcapture/client/BetPlacementClient.java
+++ client/src/main/java/com/igt/ww/betcapture/client/BetPlacementClient.java
@@ -1,6 +1,7 @@
 package com.igt.ww.betcapture.client;
 
 import java.util.*;
+import java.util.function.*;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jsr310.*;
@@ -18,10 +19,16 @@ public interface BetPlacementClient extends BetPlacementAPI {
 	@Override List<BetInfo> getAllBets();
 
 	static BetPlacementAPI client(String uri) {
+		return client(uri, null);
+	}
+
+	static BetPlacementAPI client(String uri, Consumer<Feign.Builder> clientCustomizer) {
 		List<Module> jacksonModules = List.of(new JavaTimeModule());
-		return Feign.builder()
+		Feign.Builder builder = Feign.builder()
 			.encoder(new JacksonEncoder(jacksonModules))
-			.decoder(new JacksonDecoder(jacksonModules))
-			.target(BetPlacementClient.class, uri);
+			.decoder(new JacksonDecoder(jacksonModules));
+		if (clientCustomizer != null)
+			clientCustomizer.accept(builder);
+		return builder.target(BetPlacementClient.class, uri);
 	}
 }
